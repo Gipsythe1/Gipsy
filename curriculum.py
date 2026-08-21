540562eb1bec8130c8f5cd151161c9c79a869551
Gipsy <guandarugibson@gmail.com>
2026-08-21 14:45:18 +0300

    Update curriculum.py

--- curriculum.py
+++ curriculum.py
@@ -1,14 +1,1 @@
-# curriculum.py
 
-lessons_db = {
-    "M1 W1 D1: Bid-Ask Spreads": {
-        "concept": "Order books, market makers, and liquidity mechanics.",
-        "questions": [
-            {
-                "question": "What is the primary role of a Market Maker?",
-                "options": ["Crash price", "Provide liquidity", "Charge high taxes", "Print money"],
-                "answer": "Provide liquidity"
-            }
-        ]
-    }
-}
